b494b38fda2d9af1614f859095a5bda6ecf6c39b
RastaX12 <harpascs@gmail.com>
2025-05-15 20:05:43 +0200

    script.js

--- script.js
+++ script.js
@@ -4,21 +4,15 @@ const API_URL = "https://api.themoviedb.org/3/movie/popular?api_key=" + TMDB_API
 const container = document.getElementById("movieContainer");
 
 async function loadMovies() {
-  const res = await fetch(API_URL);
-  const data = await res.json();
+  try {
+    const res = await fetch(API_URL);
+    const data = await res.json();
 
-  container.innerHTML = "";
-  data.results.forEach(movie => {
-    const card = document.createElement("div");
-    card.className = "movie-card";
-    card.innerHTML = \`
-      <img src="https://image.tmdb.org/t/p/w500\${movie.poster_path}" alt="\${movie.title}" />
-      <h3>\${movie.title}</h3>
-      <p>\${movie.release_date?.split('-')[0]}</p>
-      <a href="film.html?id=\${movie.id}">Szczegóły</a>
-    \`;
-    container.appendChild(card);
-  });
-}
-
-loadMovies();
+    container.innerHTML = "";
+    data.results.forEach((movie) => {
+      const card = document.createElement("div");
+      card.className = "movie-card";
+      card.innerHTML = `
+        <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" />
+        <h3>${movie.title}</h3>
+        <p>${movie.release_date?.split('-')[0]}</_
